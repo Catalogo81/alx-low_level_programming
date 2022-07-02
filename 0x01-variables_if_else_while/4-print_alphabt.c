2128ee304afff0bd24ccbd3e4a6038482a1c7113
Catalogo81 <peacematjato@gmail.com>
2022-07-01 19:46:50 -0700

    Script 4-print_alphabt.c added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variable c is declared of type char
+ * the for loop is responsible for printing the alphabets
+ * the if statement is responsivle for excluding letters q and e
+ *
+ * Reaturn: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
