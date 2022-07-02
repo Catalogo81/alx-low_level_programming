a6638a117cd9305c2a88686ee36289d26db42834
Catalogo81 <peacematjato@gmail.com>
2022-07-01 19:13:32 -0700

    Script 2-print_alphabet.c added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ * declare the c variable as a char
+ * uses a for loop to iterate through the alphabet from a-z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
