b4017e48ebc0f2e9bbe54f6594a4b3527df02d57
Catalogo81 <peacematjato@gmail.com>
2022-07-01 19:56:45 -0700

    Script 5-print_numbers.c added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variable n of type int is declared
+ * for loop for iterating numbers 1-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}	
+
+	printf("\n");
+	return (0);
+}
