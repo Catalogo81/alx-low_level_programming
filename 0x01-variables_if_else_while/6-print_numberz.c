7716b5dc2b233128a2c6c355eda0cf11a1fa9b7c
Catalogo81 <peacematjato@gmail.com>
2022-07-01 20:16:57 -0700

    Scripts 5-print_numbers.c and 6-print_numberz.c updated

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,11 +11,9 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
-	{
-		printf("%d", n);
-	}
+	for( n = '0'; n <= '9'; n++)
+		putchar(n);
 
-	printf("\n");
-	return (0);
+	putchar('\n');
+	return(0);
 }
