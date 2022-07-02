840abf93a4c635e777b8b47be234eb38e8553aa5
Catalogo81 <peacematjato@gmail.com>
2022-07-01 20:40:48 -0700

    Script 8-print_base16.c added

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * variable c is declared of type char
+ * for loop iterates the numbers of base 16 and another for the hexadecimal string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= '9'; c++)
+		putchar(c);
+
+	for (c = 'a'; c <= 'f; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
