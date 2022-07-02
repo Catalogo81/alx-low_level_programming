e7b473e22d058abf5baeead02d4699f2be04aee8
Catalogo81 <peacematjato@gmail.com>
2022-07-01 18:01:20 -0700

    Readme and Script 0-positive_or_negative.c added to 0x01-variables_if_else_while directory

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 
 /**
  * main - Entry point
- * 	  variable n declared
- * 	  variable n will change everytime the program is ran
- * 	  then if statement will check if n is negative, positive or 0
+ * 	variable n declared
+ * 	variable n will change everytime the program is ran
+ * 	then if statement will check if n is negative, positive or 0
  *
  * Return: Always 0 (Success)
  */
@@ -17,12 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
 	return (0);
 }
