016f17d00a935174047ed35165c400f222ec7848
Catalogo81 <peacematjato@gmail.com>
2022-07-07 14:29:03 -0700

    100-prime_factor.c updated

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,28 +7,18 @@
  */
 int main(void)
 {
-	long int n, divNumber = 2, result = 0, largestFact;
+	long int n, i;
 
 	n = 612852475143;
 
-	while (n != 0)
+	for (i = 2; i < n; i++)
 	{
-		if (n % divNumber != 0)
+		while (n % i == 0)
 		{
-			divNumber += 1;
-		}
-		else
-		{
-			largestFact = n;
-			n = n / divNumber;
-			if (n == 1)
-			{
-				printf("%ld", largestFact);
-				result = 1;
-				break;
-			}
+			n = n / i;
 		}
 	}
+	printf("%ld", i);
 	_putchar('\n');
 	return (0);
 }
