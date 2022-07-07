25d3f9d49f60adf7a6a948664afff7bb79467a88
Catalogo81 <peacematjato@gmail.com>
2022-07-07 14:11:50 -0700

    5-more_numbers.c updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,18 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i, j;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		int j;
-
 		for (j = 0; j <= 14; j++)
 		{
 			if (i >= 10)
+			{
 				_putchar((j / 10) + '0');
+			}
 			_putchar((j % 10) + '0');
 		}
-		i++;
 		_putchar('\n');
 	}
 }
