126c612c263a18f2c63271b06164cb3e1e2f6558
Catalogo81 <peacematjato@gmail.com>
2022-07-12 16:39:47 -0700

    3-puts.c added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#inclue "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: pointer to string variable
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
