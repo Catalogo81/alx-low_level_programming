6ef09ac34d834c09d92d2f74c78ecc686be9dba7
Catalogo81 <peacematjato@gmail.com>
2022-07-12 17:09:26 -0700

    4-print_rev.c added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string pointer
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, strLength;
+
+	strLength = strlen(s);
+	for (i = strLength - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
