8fcc99cc05e87574ccb4799ddfda572f538b2cf6
Catalogo81 <peacematjato@gmail.com>
2022-07-13 03:34:59 -0700

    0-strcat.c added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ * @dest: is the variable used to store destination
+ * @src: is variable or the source
+ *
+ * Return: pointer to restulting string 'dest'
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, length = 0;
+
+	while (dest[length])
+	{
+		length++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[length] = src[i];
+		length += 1;
+	}
+	dest[length] = '\0';
+	return (dest);
+}
