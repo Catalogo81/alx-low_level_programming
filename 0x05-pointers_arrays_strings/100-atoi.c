a834c44dd649111f854eb840652bc097ad06ae85
Catalogo81 <peacematjato@gmail.com>
2022-07-12 18:56:04 -0700

    100-atoi.c added

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 		else if (cnvNum > 0)
 			break;
 		else if (*s >= '0' && *s <= '9')
-			cnvNum = (cnvNum * 10) + (s[i] - '0');
+			cnvNum = (cnvNum * 10) + (*s - '0');
 	} while (s++);
 
 	return (cnvNum * sign);
