91802e43bf8f141325ecaabe32c59ba515bc9c6e
Catalogo81 <peacematjato@gmail.com>
2022-07-12 17:18:53 -0700

    4-print_rev.c added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,17 @@
  */
 void print_rev(char *s)
 {
-	int i, strLength;
+	int i, strLength = 0;
+
+	while (s[strLength] != '0')
+	{
+		strLength++;
+	}
 
 	strLength = strlen(s);
 	for (i = strLength - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
