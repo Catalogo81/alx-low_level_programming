3c500a8bcce5e798bb0c1ffcb9fd1df336513abd
Catalogo81 <peacematjato@gmail.com>
2022-07-12 18:48:35 -0700

    100-atoi.c added

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,11 +9,17 @@
 int _atoi(char *s)
 {
 	int i, cnvNum = 0;
+	int sign = 1;
 
 	for (i = 0; s[i] != '\0'; ++i)
 	{
+		if (*s == '-')
+			sign *= -1;
+		else if (cnvNum > 0)
+			break;
+
 		cnvNum = (cnvNum * 10) + (s[i] - '0');
 	}
 
-	return (cnvNum);
+	return (cnvNum * sign);
 }
