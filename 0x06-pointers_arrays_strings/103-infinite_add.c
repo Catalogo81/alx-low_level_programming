8f944e1c3ac21377599744bb4de6ebd64c562de4
Catalogo81 <peacematjato@gmail.com>
2022-07-14 14:56:37 -0700

    103-infinite_add.c and 104-print_buffer.c added

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *infinite_add - a function that adds two numbers
+ * @n1: parameter 1
+ * @n2: parameter 2
+ * @r: parameter 3
+ * @size_r: parameter 4
+ *
+ * Return: char
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buffer - a function that prints a buffer
+ * @b: first parameter
+ * @size: second parameter
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	return (0);
+}
