ae03419b974ace9c831567947187ac5ffbb1cbe5
Catalogo81 <peacematjato@gmail.com>
2022-07-18 18:19:45 -0700

    3-strspn.c updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j, k = 0;
+	unsigned int i = 0, j, k = 0, byte = 0;
 
 	while (accept[i])
 	{
 		j = 0;
+		byte = 32;
 
-		while (s[j] != 32)
+		while (s[j] != byte)
 		{
 			if (accept[i] == s[j])
 			{
 				k++;
 			}
-			j++
+			j++;
 		}
 		i++;
 	}
