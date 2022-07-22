6cc80b5680f0cd4b0212f066797a50d4e7d6abba
Catalogo81 <peacematjato@gmail.com>
2022-07-22 15:43:05 -0700

    0-whatsmyname.c updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *
  * Return: 0 Success
  */
-int main(int argc, char *argv[])
+int main(int argc attribute ((unused)), char *argv[])
 {
-	printf("%s\n'", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
