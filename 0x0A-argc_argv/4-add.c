b3eda5adba639686cfb8495c511623aefeb2a9f5
Catalogo81 <peacematjato@gmail.com>
2022-07-22 16:37:23 -0700

    4-add.c added

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: int parameter
+ * @argv: string array parameter
+ *
+ * Return: 0 (Success) 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, k, answ, num;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	for (k = 0; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		answ += num;
+	}
+	printf("%d\n", answ);
+	return (0);
+}
