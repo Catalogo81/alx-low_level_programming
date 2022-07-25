a29320caa5907620db16da661233097ddbd4e2fd
Catalogo81 <peacematjato@gmail.com>
2022-07-25 15:42:26 -0700

    1-strdup.c added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
+ * @str: string string parameter
+ *
+ * Return: Null is str = NULL, and NULL if insufficient memory
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 1;
+	while (str[i])
+	{
+		i++;
+	}
+	c = (char *)malloc(i * sizeof(char) + 1);
+	if (c == NUUL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	while (j < i)
+	{
+		c[j] = str[j];
+		j++;
+	}
+	c[j] = '\0';
+	return (c);
+}
