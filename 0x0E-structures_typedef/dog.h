9f08011787f8cf4cbce2ca91937582576cdd3158
Catalogo81 <peacematjato@gmail.com>
2022-08-01 16:03:06 -0700

    dog.h updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog struct
+ * @name: dog name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Description: Dog attributes
+ */
 struct dog
 {
 	char *name;
@@ -8,7 +16,7 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_typedef;
+typedef struct dog dog_td;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
