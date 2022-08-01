3da11fb2aeab35f4b2e817cf93289614c5297989
Catalogo81 <peacematjato@gmail.com>
2022-08-01 16:40:15 -0700

    2-print_dog.c updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,30 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		printf("");
-	}
-	if (d.name == NULL || d.age == NULL || d.owner == NULL)
+
+	if (d->name == NULL)
 	{
-		printf("%s", nil)
+		printf("Name:  (nil)\n");
 	}
 	else
 	{
 		printf("Name: %s\n", d.name);
+	}
+	if (d->age == NULL)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
 		printf("Age: %d\n", d.age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
 		printf("Owner: %s\n", d.owner);
 	}
 }
