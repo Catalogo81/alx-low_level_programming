378488fa203b8d4fe7d24bc3bec7c04393738481
Catalogo81 <peacematjato@gmail.com>
2022-08-04 04:53:59 -0700

    0-print_name.c added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name variable
+ * @f: function pointer
+ *
+ * Return: name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		if (f == NULL)
+			return;
+	}
+	f(name);
+}
